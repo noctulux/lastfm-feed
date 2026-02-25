ef460e28f9d2c631427c9d1b48e62220d7ce2302
Lux <vanhelygaming@gmail.com>
2026-02-26 01:07:06 +0700

    Update fetch.js

--- fetch.js
+++ fetch.js
@@ -1,1 +1,26 @@
+import fs from "fs";
+import fetch from "node-fetch";
 
+const USERNAME = "avechuu";
+const API_KEY = "YOUR_LASTFM_API_KEY";
+
+const url = `https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=${USERNAME}&api_key=${API_KEY}&format=json&limit=1`;
+
+async function run() {
+  const response = await fetch(url);
+  const data = await response.json();
+
+  const track = data.recenttracks.track[0];
+
+  const output = {
+    name: track.name,
+    artist: track.artist["#text"],
+    image: track.image[2]["#text"],
+    nowPlaying: track["@attr"]?.nowplaying === "true",
+    timestamp: track.date?.uts || null
+  };
+
+  fs.writeFileSync("latest.json", JSON.stringify(output, null, 2));
+}
+
+run();
